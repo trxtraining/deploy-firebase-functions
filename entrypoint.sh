05352c330e0ee1ccdfb8f5fec8388efb00c737a2
Chris Sankey <5505109+chrissank@users.noreply.github.com>
2020-06-28 17:22:31 -0400

    Add option to deploy to other target

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,13 @@ if [ -z "${FIREBASE_TOKEN}" ]; then
     exit 1
 fi
 
+if [ -z "${TARGET}" ]; then
+    echo "TARGET is missing"
+    TARGET = "default"
+fi
+
+firebase use ${TARGET}
+
 firebase deploy \
     --token ${FIREBASE_TOKEN} \
     --only functions
